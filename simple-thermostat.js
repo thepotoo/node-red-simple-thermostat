2e18132c98ed3d4bb2dfc1371c6eee97221bdfae
thepotoo <the.potoo@gmail.com>
2020-04-10 15:59:26 +0100

    Initial release

---
+++ simple-thermostat.js
@@ -0,0 +1,76 @@
+module.exports = function(RED) {
+    function simpleThermostat(config) {
+        RED.nodes.createNode(this,config);
+        // Read in settings
+        this.target = config.target;
+        this.ifAbove = config.ifAbove;
+        this.ifBelow = config.ifBelow;
+        this.equal = config.equal;
+        this.default = config.default;
+        var node = this;
+        
+        // Input handling
+        node.on('input', function(msg) {
+            try {
+                node.status({});
+                var isValid = false;
+                var currentValue = msg.payload;
+                if ( isNaN(currentValue) || Array.isArray(currentValue)) {
+                    isValid = false;
+                    // Array of values - returned via influx query, for example:
+                    // SELECT MEAN(value) FROM "My meaurement" WHERE time > now() - 2m`
+                    if (Array.isArray(currentValue) ) {
+                        try {
+                            currentValue = currentValue[0].mean;
+                            isValid = true;
+                        } catch(e) {
+                            //currentValue = 0;
+                            isValid = false;
+                            node.status({fill:"gray",shape:"dot",text:"No recent sensor readings"});
+                        }
+                    }
+                } else {
+                    // it's a number, yay!
+                    isValid = true;
+                }
+                if (isValid) {
+                    var target = "";
+                    if ( msg.hasOwnProperty('target') ) {
+                        target = msg.target;
+                    } else {
+                        target = node.target;
+                    }
+                    if ( isNaN(target) || target === "") {
+                        isValid = false;
+                        node.status({fill:"gray",shape:"dot",text:"Target must be a number"});
+                    }
+                }
+                if (isValid) {
+                    if (currentValue > target) {
+                        node.status({fill:"green",shape:"dot",text: `${currentValue.toFixed(2)} greater than ${target}`});
+                        msg.payload = node.ifAbove;
+                    } else if (currentValue < target){
+                        node.status({fill:"red",shape:"dot",text: `${currentValue.toFixed(2)} less than ${target}`});
+                        msg.payload = node.ifBelow;
+                    } else if (currentValue === target) {
+                        node.status({fill:"gray",shape:"dot",text: `Payload equal to ${target}`});
+                        msg.payload = node.equal;
+                    } else {
+                        msg.payload = node.default;
+                    }
+                } else {
+                    // Either no readings or msg.target was not a number
+                    msg.payload = node.default;
+                }
+                // "" means return nothing
+                if (msg.payload !== "") {
+                    node.send(msg)
+                }
+            } catch (err) {
+                node.status({fill:"gray",shape:"dot",text:err}); 
+                node.warn(err);
+            }
+        });
+    }
+    RED.nodes.registerType("simple thermostat",simpleThermostat);
+}
---
+++ test/_spec.js
@@ -0,0 +1,117 @@
+var should = require("should");
+var helper = require("node-red-node-test-helper");
+var thermostat = require("../simple-thermostat.js");
+helper.init(require.resolve('node-red'));
+
+describe('simple thermostat Node', function () {
+
+  afterEach(function () {
+    helper.unload();
+  });
+
+  it('should be loaded', function (done) {
+    var flow = [{ id: "n1", type: "simple thermostat", name: "test name" }];
+    helper.load(thermostat, flow, function () {
+      var n1 = helper.getNode("n1");
+      n1.should.have.property('name', 'test name');
+      done();
+    });
+  });
+
+  it('should return off if greater than msg.payload', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "turn off if above 5", ifAbove: "off", target: 5, wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'off');
+          done();
+        });
+        n1.receive({ payload: 6 });
+    });
+  });
+
+  it('should allow overriding with msg.target', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "target will be overridden", ifAbove: "off", ifBelow: "on", target: 10, wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'off');
+          done();
+        });
+        n1.receive({ payload: 8, target: 5 });
+    });
+  });
+
+  it('should allow use of influxdb mean format output', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "turn off if above 5", ifAbove: "off", target: 5, wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'off');
+          done();
+        });
+        n1.receive({ payload: [{'mean': 6}]});
+    });
+  });
+
+  it('should return a default value there are no recent readings from influx', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "turn off if above 5", ifAbove: "off", target: 5, default: "default payload", wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'default payload');
+          done();
+        });
+        n1.receive({ payload: []}); // If there are no readings, Influx returns an empty array as the payload
+    });
+  });
+
+  it('should return a default value if the payload can\'t be parsed', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "turn off if above 5", ifAbove: "off", target: 5, default: "default payload", wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'default payload');
+          done();
+        });
+        n1.receive({ payload: 'invalid'});
+    });
+  });
+
+  it('should return a default value if target not set', function (done) {
+    var flow = [
+        { id: "n1", type: "simple thermostat", name: "turn off if above 5", ifEqual: "off", target: "", default: "default payload", wires:[["n2"]] },
+        { id: "n2", type: "helper" }
+    ];
+    helper.load(thermostat, flow, function () {
+        var n2 = helper.getNode("n2");
+        var n1 = helper.getNode("n1");
+        n2.on("input", function (msg) {
+          msg.should.have.property('payload', 'default payload');
+          done();
+        });
+        n1.receive({ payload: 0});
+    });
+  });
+
+});
